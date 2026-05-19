57a37598e8db225934ab3174c41e6ea2a2d482c1
sharan21G <babugm987@gmail.com>
2026-05-19 08:44:25 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,2 +1,11 @@
 amazon-linux-extras install ansible2 -y
 yum install python3 python-pip python-dlevel -y
+ansible --version
+ansible -m ping localhost
+ansible localhost -a "yum install git -y"
+ansible localhost -a "git --version"
+ansible localhost -a "mvn --version"
+ansible localhost -a "touch file1"
+ansible localhost -a "ls"
+ansible localhost -a "yum install httpd -y"
+ansible localhost -a "systemctl status httpd"
