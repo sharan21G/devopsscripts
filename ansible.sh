464ffc4e8742d9042faebe0da6afe3af334ae315
sharan21G <babugm987@gmail.com>
2026-05-20 07:44:34 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -1,6 +1,9 @@
 amazon-linux-extras install ansible2 -y
 yum install python3 python-pip python-dlevel -y
 ansible --version
+passwd root
+root123456
+vi /etc/ssh/sshd_config
 ansible -m ping localhost
 ansible localhost -a "yum install git -y"
 ansible localhost -a "git --version"
